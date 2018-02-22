985067ed6f99790df1aa55156d3cc256520be85e
MALANG <MALANG@KHURRAM>
2018-02-22 13:03:03 +0500

    fsdsdf

---
+++ Assignment_1/Controllers/AccountsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public string Fees(int id = 923073 , string name = "Khurram" ) 
+        {
+            var yes = "yes";
+            var feespay = yes;
+           
+            if (feespay == yes)
+            {
+                return "You are allowed to take classes";
+            }
+            else
+            {
+                return "You are not allowed to take classes becouse your fess is not payed";
+            }
+        }
+    }
+}
---
+++ Assignment_1/Controllers/AdmissionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    public class AdmissionController : Controller
+    {
+        // GET: Admission
+        
+          public string Index(string name = "Khurram")
+        {
+            string batch = "201510C";
+            var id = 923073;
+            var yes = "yes";
+            var alive = yes;
+            
+            if (alive == yes)
+
+                return name+" You are enrolled in "+batch+" and your id is "+id;
+            else
+
+                return name + " Sorry We are not able to enrolle you because your not alive ";
+        }
+        public string CancelAdmission(int id = 923073 , string name = "Khurram")
+        {
+            return "Your Admission is cancelled";
+        }
+    }
+}
